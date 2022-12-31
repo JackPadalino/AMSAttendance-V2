c2c1053c96ab8ba6d400934ae3da539a2c021b95
JackPadalino <pada0867@gmail.com>
2022-12-30 20:22:13 -0500

    added forms to admin component to select absent teachers and letter day

--- src/components/AdminAbsences.js
+++ src/components/AdminAbsences.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { NotFoundPage } from ".";
-import { setDate,setLetterDay,setDay,setAllAbsences,setCoveredClasses,resetCoveredClasses } from "../store/adminSlice";
+import { setDate,setLetterDay,setDay,setAllAbsentUsers,setCoveredClasses,resetCoveredClasses } from "../store/adminSlice";
 import { useSelector, useDispatch } from "react-redux";
 
 const AdminAbsences = () => {
     const dispatch = useDispatch();
-    const { date,letterDay,day,coveredClasses,allAbsences } = useSelector((state) => state.admin);
+    const { date,letterDay,day,coveredClasses,allAbsentUsers } = useSelector((state) => state.admin);
     const [token, setToken] = useState(window.localStorage.getItem("token"));
     const [teacherId,setTeacherId] = useState(0);
 
@@ -32,7 +32,7 @@ const AdminAbsences = () => {
         const userPromises = absences.data.map(absence => axios.get(`/api/users/${absence.user.id}`));
         const userResponses = await Promise.all(userPromises);
         const userAbsences = userResponses.map(response => response.data);
-        dispatch(setAllAbsences(userAbsences));
+        dispatch(setAllAbsentUsers(userAbsences));
         dispatch(resetCoveredClasses());
     };
 
@@ -45,28 +45,28 @@ const AdminAbsences = () => {
     if(!token) return <NotFoundPage/>
     return (
         <div>
+            <h3>Absences {date}</h3>
             <form onSubmit={getAbsences}>
                 <label htmlFor="date">Date</label>
                 <input type="date" id="date" value={date} onChange={handleDateChange}></input>
                 <input type='submit' value='Submit'/>
             </form>
-            <h3>Absences {date}</h3>
             <div>
-                {allAbsences.length>0 && allAbsences.map((user) => {
+                {allAbsentUsers.length>0 && allAbsentUsers.map((user) => {
                     return (
                         <div key={user.id}>
                             <p>{user.firstName} {user.lastName}</p>
                         </div>  
                     );
                 })}
-                {allAbsences.length===0 && <p>Looks like there is no information about this date.</p>}
+                {allAbsentUsers.length===0 && <p>Looks like there is no information about this date.</p>}
             </div>
             <h3>Needed coverages</h3>
             <form onSubmit={getClasses}>
                 <label htmlFor="teacher">Teacher</label>
                 <select name="teacher" id="teacher" onChange={handleTeacherChange}>
                     <option value="-">-</option>
-                    {allAbsences.map((teacher) => {
+                    {allAbsentUsers.map((teacher) => {
                         return (
                             <option key={teacher.id} value={teacher.id}>{teacher.firstName} {teacher.lastName}</option>
                         );
@@ -88,7 +88,8 @@ const AdminAbsences = () => {
                 {coveredClasses.map((eachClass) => {
                     return (
                         <div key={eachClass.id}>
-                            <p>{eachClass.name}</p>
+                            {/* <p>{eachClass.name}</p> */}
+                            <Link to={`/admin/coverages/${eachClass.school}/${eachClass.period}/${letterDay}`}>{eachClass.name} - {eachClass.period}</Link>
                         </div>  
                     );
                 })}
