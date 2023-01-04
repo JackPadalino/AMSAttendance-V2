d6065b03e33fc145952ddd3acd3441610de1ffb5
JackPadalino <pada0867@gmail.com>
2023-01-04 00:19:10 -0500

    updated component to create new class

--- src/components/Classes/Classes.js
+++ src/components/Classes/Classes.js
@@ -2,45 +2,73 @@ import axios from 'axios';
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { NotFoundPage } from "..";
-import { setDate,setDay,setAllAbsences } from "../../store/absenceSlice";
+import { setAllClasses } from "../../store/classSlice";
+import { set,setDay,setAllAbsences } from "../../store/absenceSlice";
 import { useSelector, useDispatch } from "react-redux";
-
-const formStyle={
-    display:'flex',
-    flexDirection:'column'
-};
-
-const formTop = {
-    display:'flex',
-    gap:'10px',
-    marginBottom:'10px'
-};
-
-const formBottom = {
-    display:'flex',
-    gap:'10px',
-    marginBottom:'10px'
-};
 
 const Classes = () => {
     const dispatch = useDispatch();
     const [token, setToken] = useState(window.localStorage.getItem("token"));
+    const [name,setName] = useState('');
+    const [school,setSchool] = useState('');
+    const [grade,setGrade] = useState('');
+    const [period,setPeriod] = useState('');
+    const [letterDays,setLetterDays] = useState([]);
     
+    const addClass = async(event) =>{
+        event.preventDefault();
+        const body = {
+            name,
+            school,
+            grade,
+            period,
+            letterDays
+        };
+        await axios.post(`/api/classes`,body);
+        const allClasses = await axios.get('/api/classes');
+        dispatch(setAllClasses(allClasses.data));
+    };
+
+    const handleNameChange = (event) =>{
+        setName(event.target.value);
+    };
+
+    const handleSchoolChange = (event) =>{
+        setSchool(event.target.value);
+    };
+
+    const handleGradeChange = (event) =>{
+        setGrade(event.target.value);
+    };
+
+    const handlePeriodChange = (event) =>{
+        setPeriod(event.target.value);
+    };
+
+    const handleLetterDaysChange =(event)=>{
+        if(letterDays.includes(event.target.value)){
+            setLetterDays(letterDays.filter(day=>day!==event.target.value))
+        }else{
+            setLetterDays([...letterDays,event.target.value]);
+        };
+    };
+
+
     if(!token) return <NotFoundPage/>
     return (
         <div>
             <h1>Add a class</h1>
-            <form>
-                <div style={formTop}>
-                    <input placeholder="Class name" />
+            <form onSubmit={addClass}>
+                <div>
+                    <input placeholder="Class name" onChange={handleNameChange}/>
                     <label htmlFor="school">MS/HS</label>
-                    <select name='school'>
+                    <select name='school' onChange={handleSchoolChange}>
                         <option value="-">-</option>
                         <option value="MS">MS</option>
                         <option value="HS">HS</option>
                     </select>
                     <label htmlFor="grade">Grade</label>
-                    <select name='grade'>
+                    <select name='grade' onChange={handleGradeChange}>
                         <option value="-">-</option>
                         <option value="6">6</option>
                         <option value="7">7</option>
@@ -50,8 +78,8 @@ const Classes = () => {
                         <option value="11">11</option>
                         <option value="12">12</option>
                     </select>
-                    <label htmlFor="period">Grade</label>
-                    <select name='period'>
+                    <label htmlFor="period">Period</label>
+                    <select name='period' onChange={handlePeriodChange}>
                         <option value="-">-</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
@@ -62,19 +90,19 @@ const Classes = () => {
                         <option value="7">7</option>
                     </select>
                 </div>
-                <div style={formBottom}>
-                    <input type="checkbox" name="A day" value="A"/>
-                    <label for="A day">A day</label>
-                    <input type="checkbox" name="B day" value="B"/>
-                    <label for="B day">B day</label>
-                    <input type="checkbox" name="C day" value="C"/>
-                    <label for="C day">C day</label>
-                    <input type="checkbox" name="D day" value="D"/>
-                    <label for="D day">D day</label>
-                    <input type="checkbox" name="E day" value="E"/>
-                    <label for="E day">E day</label>
-                    <input type="checkbox" name="F day" value="F"/>
-                    <label for="F day">F day</label>
+                <div>
+                    <input type="checkbox" name="A day" value="A" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="A day">A</label>
+                    <input type="checkbox" name="B day" value="B" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="B day">B</label>
+                    <input type="checkbox" name="C day" value="C" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="C day">C</label>
+                    <input type="checkbox" name="D day" value="D" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="D day">D</label>
+                    <input type="checkbox" name="E day" value="E" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="E day">E</label>
+                    <input type="checkbox" name="F day" value="F" onChange={handleLetterDaysChange}/>
+                    <label htmlFor="F day">F</label>
                 </div>
                 <button>Submit</button>
             </form>
